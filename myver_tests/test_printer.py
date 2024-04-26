5b61afdee6a3f3171ce0f2043c183f0e214fbf42
Kiara Fiorella Abad Bruzzo <kabadbruzzo@gmail.com>
2024-04-26 23:11:57 +0200

    find printer settings

--- myver_tests/test_printer.py
+++ myver_tests/test_printer.py
@@ -1,5 +1,17 @@
 from escpos import printer
+import usb
+
+# find your printer vendor id and product id, needed to initialize printer via usb
+
+busses = usb.busses()
+for bus in busses:
+    devices = bus.devices
+    for dev in devices:
+        print("Device:", dev.filename)
+        print("    idVendor: %d (0x%04x)" %(dev.idVendor, dev.idVendor))
+        print("    idProduct: %d (0x%04x)" %(dev.idProduct, dev.idProduct))
+
 
 ## initialize printer
-p = printer.Serial("/dev/tty0") ## todo: change serial code
-p.test("Hello World \n")
+p = printer.Usb(0x28e9, 0x0289, in_ep=0, out_ep=0x03) ## todo: change serial code
+p.text("Hello World \n")
